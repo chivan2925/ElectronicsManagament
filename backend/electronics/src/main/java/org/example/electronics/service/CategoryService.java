7f552498af3dc1d711671b49017a887df20fbe3f
KhanggCloud <vuhongvinhkhang@gmail.com>
2026-03-07 20:18:40 +0700

    Add CategoryService Interface and new DBML checkout from main

--- backend/electronics/src/main/java/org/example/electronics/service/CategoryService.java
+++ backend/electronics/src/main/java/org/example/electronics/service/CategoryService.java
@@ -1,4 +1,22 @@
 package org.example.electronics.service;
 
-public class CategoryService {
+import org.example.electronics.dto.request.CategoryRequestDTO;
+import org.example.electronics.dto.response.CategoryResponseDTO;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
+public interface CategoryService {
+
+    //Admin
+    CategoryResponseDTO createCategory(CategoryRequestDTO categoryRequestDTO);
+    CategoryResponseDTO updateCategory(Integer categoryId, CategoryRequestDTO categoryRequestDTO);
+    void deleteCategory(Integer categoryId);
+    Page<CategoryResponseDTO> getAllParentCategories(Pageable pageable);
+    Page<CategoryResponseDTO> getAllSubCategories(Integer parentId, Pageable pageable);
+
+    //End-user
+    List<CategoryResponseDTO> getCategoryList();
+    CategoryResponseDTO getCategoryById(Integer categoryId);
 }
---
+++ backend/electronics/src/main/java/org/example/electronics/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,6 @@
+package org.example.electronics.service.impl;
+
+import org.example.electronics.service.CategoryService;
+
+public class CategoryServiceImpl implements CategoryService {
+}
